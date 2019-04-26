fc812c53c4e1b6adaf1e99e7f2d0fae45ea008b5
Stacy Nowak <nowak.stacy@gmail.com>
2019-04-26 16:26:27 -0700

    gameboard as functional component

---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./style.css";
+
+function Card() {
+    
+    return (
+        <div class="card">
+            <div class="card-body">
+                {/* image here */}
+            </div>
+        </div>
+    )
+}
+
+export default Card;
