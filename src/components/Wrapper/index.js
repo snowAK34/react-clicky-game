e8d51a644e00846f022da3114436ccc69fc454df
Stacy Nowak <nowak.stacy@gmail.com>
2019-04-26 16:19:15 -0700

    listed score and button in navbar, changed wrapper to class

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import NavBar from "./components/NavBar";
 import Jumbotron from "./components/Jumbotron";
 import Gameboard from "./components/Gameboard";
 import "./style.css";
 
-function Wrapper() {
-    return (
-        <div  className= "container-fluid">
-            <NavBar />
-            <Jumbotron />
-            <Gameboard />
-        </div>
-    )
+class Wrapper extends Component {
+    state = {
+        score: 0,
+        topScore: 0,
+        cards: []
+    }
+
+    
+
+    render() {
+        return (
+            <div  className= "container-fluid">
+                <NavBar />
+                <Jumbotron />
+                <Gameboard />
+            </div>
+        )
+    }
 }
 
 export default Wrapper;
