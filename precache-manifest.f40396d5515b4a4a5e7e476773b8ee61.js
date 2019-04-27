247919fe68f52583e8f950b12a9ceaef2fe8d3e5
Stacy Nowak <nowak.stacy@gmail.com>
2019-04-26 22:12:00 -0700

    Updates

---
+++ precache-manifest.f40396d5515b4a4a5e7e476773b8ee61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372809849214fa7aaa02ca232c46c5af",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "6c00908fa8244b7927d4",
+    "url": "/react-clicky-game/static/css/main.28310095.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-clicky-game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6c00908fa8244b7927d4",
+    "url": "/react-clicky-game/static/js/main.7d9836b5.chunk.js"
+  },
+  {
+    "revision": "a3b74089770802ae2f4d",
+    "url": "/react-clicky-game/static/js/runtime~main.a57faf00.js"
+  }
+]);
